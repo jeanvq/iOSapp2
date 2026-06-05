978a15afdf5a26140d7b123275db0819ab8b89f3
Jeancarlo <jeancarlo.ricardo@triosstudent.com>
2026-06-04 20:02:03 -0400

    Initial Commit

---
+++ iOSApp2/iOSApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp2App.swift
+//  iOSApp2
+//
+//  Created by Jeancarlo on 2026-06-04.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSApp2Tests/iOSApp2Tests.swift
@@ -0,0 +1,19 @@
+//
+//  iOSApp2Tests.swift
+//  iOSApp2Tests
+//
+//  Created by Jeancarlo on 2026-06-04.
+//
+
+import Testing
+@testable import iOSApp2
+
+struct iOSApp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
